8c282318f4b2b83afec480e6d67a8d3c5dde2419
Seudonym <wk170179@gmail.com>
2026-03-14 10:41:06 +0530

    chore(frontend): setup tailwind with trunk for frontend

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use leptos::prelude::*;
 fn main() {
     console_error_panic_hook::set_once();
     leptos::mount::mount_to_body(|| {
-        view! { <p>"Hello, world!"</p> }
+        view! {
+            <main class="flex items-center justify-center min-h-screen bg-slate-950 text-slate-100">
+                <h1 class="text-5xl font-semibold tracking-tight text-white sm:text-6xl">Beacon</h1>
+            </main>
+        }
     })
 }
